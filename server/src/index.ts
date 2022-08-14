70e142d1e1242ec50eee9a6fb169cc566cb03f22
madhu-srti <madhu.jayavarapu@securiti.ai>
2022-08-14 19:12:22 +0530

    BAT-1: postgres db connection added to bat project

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,22 @@ import Express from 'express';
 import "reflect-metadata";
 import { PROJ_CNST } from './constants/proj.cnst';
 import { initGraphql } from './graphql'; 
+import { AppDataSource } from './db';
 
 (async () => {
     // Initialize express app
     const app = Express();
+
+    // Initialize postgres db
+    // to initialize initial connection with the database, register all entities
+    // and "synchronize" database schema, call "initialize()" method of a newly created database
+    // once in your application bootstrap
+    try {
+        await AppDataSource.initialize();
+        console.log(`Successfully connected to db`);
+    }catch(err) {
+        console.error('Error while connecting to db: ', err);
+    }
 
     // Initialize Apollo Graphql Server
     const apolloServer = await initGraphql();
