0a99a5594491aefa107aa3c7493479e20cf7d4b8
madhu-srti <madhu.jayavarapu@securiti.ai>
2022-08-14 15:29:17 +0530

    P1: added express to the application and dev script added to package.json

---
+++ server/src/constants/proj.cnst.ts
@@ -0,0 +1,4 @@
+// TODO: Should read config from env.
+export const PROJ_CNST = {
+    PORT: 5000
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,21 @@
-console.log("index file");
+import Express from 'express';
+import { PROJ_CNST } from './constants/proj.cnst';
+
+(async () => {
+    // Initialize express app
+    const app = Express();
+
+    // Heartbeat route.
+    app.get('/', (_, res) => {
+        res.send("Server Alive!");
+    })
+
+    // Start express Server.
+    app.listen(PROJ_CNST.PORT, (err) => {
+        if(err) {
+            console.error("Error while starting the server: ", err);
+            return;
+        }
+        console.log(`Server started on PORT: http://localhost:${PROJ_CNST.PORT}`);
+    })
+})()
