1d4d555938d57b91298e86cb1661f9b5909547f8
madhujayavarapu <madhujayavarapu523@gmail.com>
2022-08-15 13:25:11 +0530

    BAT-1: scss added to project

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import { ApolloProvider } from '@apollo/client';
 import {client} from './apollo-client';
